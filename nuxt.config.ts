4ebcfa4e947e6f485e86d0a9e5ac84165548ddf6
Enea <eneazani2005@gmail.com>
2024-12-06 13:03:35 +0100

    Added basic configuration and styling setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Planify - Travel Planner',
+      meta: [
+        { name: 'description', content: 'Crea e condividi itinerari di viaggio dinamici!' }
+      ]
+    }
+  },
+  css: ['@/assets/styles/main.css'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
-})
+});
